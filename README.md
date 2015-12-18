af44f82ad2e8ae13aceb0e619b8e2bbc932de1fb
jorgescalona <escajorge@gmail.com>
2015-12-18 15:17:14 -0430

    commit inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+# bachacolibro
+repo para montar los avances con el libro de Bachaco-VE sobre Odoo
+
+prueba 2
---
+++ __openerp__.py
@@ -0,0 +1,10 @@
+{
+	'name':'To-Do application',
+	'description':'Administra tus tareas personales con este modulo',
+	'author':'jorgescalona @jorgemustaine',
+	'depends':['mail'],
+	'application':True,
+	'version':'1.0',
+	'website':'www.attakatara.wordpress.com',
+	'data':['todo_view.xml','security/ir.model.access.csv'],
+}
